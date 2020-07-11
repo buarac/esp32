0cf35eff515a517be92c0389a7dce733314b0001
Sacha Stefanovic <sacha@MacBook-Pro-de-Sacha.local>
2020-07-11 12:15:24 +0200

    Ajout configuration sdk

--- applications/bme280_sensor/main/bme280_sensor.c
+++ applications/bme280_sensor/main/bme280_sensor.c
@@ -2,9 +2,10 @@
 #include "i2c_device.h"
 
 static const char* TAG = "bme280_sensor";
-
+#define BME280_I2C_PORT CONFIG_BME280_I2C_PORT
+#define BME280_I2C_ADDR CONFIG_BME280_I2C_ADDR
 
 void app_main(void) {
-    ESP_LOGV(TAG, "app_main");
+    ESP_LOGV(TAG, "app_main on port: %d with address: %d, 0x%02x", BME280_I2C_PORT, BME280_I2C_ADDR, BME280_I2C_ADDR);
     i2c_device_info();
 }
