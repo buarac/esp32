40d32b8b78d95696c0c016246f2d229b2f9bc2c6
Sacha Stefanovic <sacha@MacBook-Pro-de-Sacha.local>
2020-07-20 19:29:00 +0200

    a

--- applications/espnow_sensor/main/espnow_sensor.c
+++ applications/espnow_sensor/main/espnow_sensor.c
@@ -27,6 +27,7 @@
 #include "esp_system.h"
 #include "esp_now.h"
 #include "esp_crc.h"
+#include "esp_sleep.h"
 
 
 /* ESPNOW can work in both station and softap mode. It is configured in menuconfig. */
@@ -41,32 +42,39 @@
 #define ESPNOW_QUEUE_SIZE           20
 
 typedef enum {
-    MASTER_EVENT_SEND_CB = 0x00,
-    MASTER_EVENT_RECV_CB = 0x80
-} master_event_type_t;
+    SENSOR_UNDEFINED_STATE = 0,
+    SENSOR_NOT_CONFIGURED ,
+    SENSOR_CONFIGURED,
+    SENSOR_CAPTURING_DATA,
+    SENSOR_CAPTURE_DONE,
+    SENSOR_SENDING_DATA,
+    SENSOR_SEND_DATA_DONE
+
+} sensor_state_t;
 
 typedef struct {
-    master_event_type_t type;
-    uint8_t             addr[ESP_NOW_ETH_ALEN];
-    uint8_t             status;
-    uint8_t             len;
-    uint8_t*            data;
-} master_event_t;
+    sensor_state_t  prev_state;
+    sensor_state_t  state;
+} sensor_event_t;
 
 typedef struct {
     float   temp;
     float   humi;
     float   pres;
-} meteo_event_t;
+} sensor_info_t;
 
-static const char *TAG = "espnow_master";
+static const char *TAG = "espnow_sensor";
 
-static xQueueHandle master_queue;
+static xQueueHandle sensor_queue;
 
-static uint8_t s_broadcast_addr[ESP_NOW_ETH_ALEN] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
+static RTC_DATA_ATTR uint8_t master_addr[ESP_NOW_ETH_ALEN] = { 0, 0, 0, 0, 0, 0 };
+static uint8_t broadcast_addr[ESP_NOW_ETH_ALEN] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
+static uint8_t null_addr[ESP_NOW_ETH_ALEN] = { 0, 0, 0, 0, 0, 0 };
+
+static sensor_state_t state = SENSOR_UNDEFINED_STATE;
+static sensor_info_t  info;
 
 static char tmp_mac_addr[20];
-
 static inline void format_mac_addr(uint8_t* mac_addr) {
     sprintf(tmp_mac_addr,
         "%02x:%02x:%02x:%02x:%02x:%02x",
@@ -79,8 +87,23 @@ static inline void format_mac_addr(uint8_t* mac_addr) {
     );
 }
 
-static inline uint8_t is_broadcast_addr(uint8_t* addr) {
-    return (memcmp(addr, s_broadcast_addr, ESP_NOW_ETH_ALEN) == 0);
+static inline uint8_t IS_NULL_ADDR(uint8_t* addr) {
+    return (memcmp(addr, null_addr, ESP_NOW_ETH_ALEN) == 0);
+}
+
+static inline uint8_t IS_BROADCAST_ADDR(uint8_t* addr) {
+    return (memcmp(addr, broadcast_addr, ESP_NOW_ETH_ALEN) == 0);
+}
+
+static void set_sensor_state(sensor_state_t new_state) {
+    sensor_event_t evt;
+
+    evt.prev_state = state;
+    state = new_state;
+    evt.state = new_state;
+    if ( xQueueSend(sensor_queue, &evt, portMAX_DELAY) != pdTRUE ) {
+        ESP_LOGW(TAG, "set_sensor_state error: send queue fail");
+    }
 }
 
 /* ESPNOW sending or receiving callback function is called in WiFi task.
@@ -92,14 +115,20 @@ static void app_espnow_send_cb(const uint8_t *mac_addr, esp_now_send_status_t st
         ESP_LOGE(TAG, "send cb error: mac_addr is null");
         return;
     }
-
+    /*
     master_event_t evt;
     evt.type = MASTER_EVENT_SEND_CB;
     evt.status = status;
     memcpy(&evt.addr, mac_addr,ESP_NOW_ETH_ALEN);
 
-    if (xQueueSend(master_queue, &evt, portMAX_DELAY) != pdTRUE) {
+    if (xQueueSend(sensor_queue, &evt, portMAX_DELAY) != pdTRUE) {
         ESP_LOGW(TAG, "send cb error: send queue fail");
+    }
+    */
+    ESP_LOGI(TAG, "app_espnow_send_cb(status=%d)", status);
+    ESP_LOGI(TAG, "current sensor state %d", state);
+    if ( state > SENSOR_NOT_CONFIGURED ) {
+        set_sensor_state(SENSOR_SEND_DATA_DONE);
     }
 }
 
@@ -109,7 +138,7 @@ static void app_espnow_recv_cb(const uint8_t *mac_addr, const uint8_t *data, int
         ESP_LOGE(TAG, "receive cb error: bad arguments");
         return;
     }
-
+    /*
     master_event_t evt;
     evt.type = MASTER_EVENT_RECV_CB;
     memcpy(&evt.addr, mac_addr, ESP_NOW_ETH_ALEN);
@@ -120,22 +149,37 @@ static void app_espnow_recv_cb(const uint8_t *mac_addr, const uint8_t *data, int
     }
     memcpy(evt.data, data, len);
     evt.len = len;
-    if (xQueueSend(master_queue, &evt, portMAX_DELAY) != pdTRUE) {
+    if (xQueueSend(sensor_queue, &evt, portMAX_DELAY) != pdTRUE) {
         ESP_LOGW(TAG, "receive cb error: send queue fail");
         free(evt.data);
     }
+    */
+    ESP_LOGI(TAG, "app_espnow_recv_cb(len=%d)", len);
+    memcpy(master_addr, mac_addr, ESP_NOW_ETH_ALEN);
+    set_sensor_state(SENSOR_CONFIGURED);
 }
 
 
 
-static esp_err_t app_add_pear(uint8_t* addr) {
+static void do_sensor_configuration() {
+    if ( IS_NULL_ADDR(master_addr) ) {
+        uint16_t ping = 1973;
+        esp_now_send(broadcast_addr, (void*)&ping, 2);
+    }
+    else {
+        set_sensor_state(SENSOR_CONFIGURED);
+    }
+}
+
+
+static esp_err_t app_add_peer(uint8_t* addr) {
     /* Add broadcast peer information to peer list. */
      esp_err_t ret = ESP_OK;
     if ( esp_now_is_peer_exist(addr)  == false ) {
         esp_now_peer_info_t *peer = malloc(sizeof(esp_now_peer_info_t));
         if (peer == NULL) {
             ESP_LOGE(TAG, "Malloc peer information fail");
-            vSemaphoreDelete(master_queue);
+            vSemaphoreDelete(sensor_queue);
             esp_now_deinit();
             return ESP_FAIL;
         }
@@ -154,45 +198,6 @@ static esp_err_t app_add_pear(uint8_t* addr) {
     return ret;
 }
 
-static void app_espnow_task(void *pvParameter) {
-
-    master_event_t evt;
-
-    while (xQueueReceive(master_queue, &evt, portMAX_DELAY) == pdTRUE) {
-        printf("------------- <NEW EVENT> ---------------\n");
-        if ( evt.type == MASTER_EVENT_SEND_CB ) {
-            format_mac_addr(evt.addr);
-            printf("event sent to [%s] with status = %d\n", tmp_mac_addr, evt.status);
-        }
-        else if ( evt.type == MASTER_EVENT_RECV_CB ) {
-            format_mac_addr(evt.addr);
-            printf("event received from [%s] with %d byte(s)\n", tmp_mac_addr, evt.len);
-            ESP_LOG_BUFFER_HEXDUMP(TAG, evt.data, evt.len, ESP_LOG_WARN);
-            if ( evt.len == sizeof(meteo_event_t)) {
-                meteo_event_t* m_evt = (meteo_event_t*)evt.data;
-                printf("temperature: %.2f\n", m_evt->temp);
-                printf("humidity   : %.2f\n", m_evt->humi);
-                printf("pressure   : %.2f\n", m_evt->pres);                
-            } 
-            else if ( evt.len == 2 ) {
-                uint16_t code = (uint16_t)(*evt.data | (*(evt.data+1) << 8));
-                ESP_LOGI(TAG, "ping event from %s with code %d", tmp_mac_addr, code);
-                if ( code == 1973 ) {
-                    //ESP_LOGI(TAG, "ping event from %s", tmp_mac_addr);
-                    // ping
-                    code = 1389;
-                    // renvoyer le ping
-                    //esp_now_send ( evt.addr, (void*)&code, 2);
-                    app_add_pear ( evt.addr );
-                    esp_now_send ( s_broadcast_addr, (void*)&code, 2);
-                }
-            }
-            free(evt.data);
-        }
-    }
-}
-
-
 
 /* WiFi should start before using ESPNOW */
 static void app_wifi_init(void)
@@ -208,8 +213,8 @@ static void app_wifi_init(void)
 
 static esp_err_t app_espnow_init(void) {
 
-    master_queue = xQueueCreate(ESPNOW_QUEUE_SIZE, sizeof(master_event_t));
-    if (master_queue == NULL) {
+    sensor_queue = xQueueCreate(ESPNOW_QUEUE_SIZE, sizeof(sensor_event_t));
+    if (sensor_queue == NULL) {
         ESP_LOGE(TAG, "Create mutex fail");
         return ESP_FAIL;
     }
@@ -219,9 +224,61 @@ static esp_err_t app_espnow_init(void) {
     ESP_ERROR_CHECK( esp_now_register_send_cb(app_espnow_send_cb) );
     ESP_ERROR_CHECK( esp_now_register_recv_cb(app_espnow_recv_cb) );
 
-    ESP_ERROR_CHECK( app_add_pear(s_broadcast_addr) );
+    ESP_ERROR_CHECK( app_add_peer(broadcast_addr) );
+
+    format_mac_addr(master_addr);
+    ESP_LOGI(TAG, "master mac addr: %s", tmp_mac_addr);
 
     return ESP_OK;
+}
+
+static void do_capture_data() {
+    info.temp = 19.73;
+    info.humi = 13.89;
+    info.pres = 1013.25;
+    set_sensor_state(SENSOR_CAPTURE_DONE);
+}
+
+static void do_send_data() {
+    ESP_LOGI(TAG, "do_send_data()");
+    esp_now_send(master_addr, (void*)&info, sizeof(sensor_info_t));
+}
+
+static void do_deep_sleep() {
+    ESP_LOGI(TAG, "Enabling timer wakeup, %ds\n", 30);
+    esp_sleep_enable_timer_wakeup(30 * 1000000);
+    esp_deep_sleep_start();
+}
+
+static void sensor_event_handler(void *pvParameter) {
+
+    sensor_event_t evt;
+
+    while (xQueueReceive(sensor_queue, &evt, portMAX_DELAY) == pdTRUE) {
+        printf("------------- <NEW SENSOR EVENT> ---------------\n");
+        printf("sensor state changed %d -> %d\n", evt.prev_state, evt.state);
+        switch (evt.state) {
+            case SENSOR_NOT_CONFIGURED:
+                do_sensor_configuration();
+                break;
+            case SENSOR_CONFIGURED:
+                format_mac_addr(master_addr);
+                ESP_LOGI(TAG, "master addr [%s]", tmp_mac_addr);
+                app_add_peer(master_addr);
+                do_capture_data();
+                break;
+            case SENSOR_CAPTURE_DONE:
+                ESP_LOGI(TAG, "capture data done");
+                do_send_data();
+                break;
+            case SENSOR_SEND_DATA_DONE:
+                ESP_LOGI(TAG, "data send done");
+                do_deep_sleep();
+                break;
+            default:
+                ESP_LOGI(TAG, "state %d not processed yet", evt.state);
+        }
+    }
 }
 
 void app_main(void)  {
@@ -236,6 +293,7 @@ void app_main(void)  {
     app_wifi_init();
     app_espnow_init();
 
+    xTaskCreate(sensor_event_handler, "sensor_event_handler", 2048, NULL, 4, NULL);
 
-    xTaskCreate(app_espnow_task, "app_espnow_task", 2048, NULL, 4, NULL);
+    set_sensor_state(SENSOR_NOT_CONFIGURED);
 }
